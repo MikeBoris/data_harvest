bda61f774f3b2ef58ebc84889cec764dc0e27e3c
MikeBoris <mikeboris123@gmail.com>
2018-01-15 21:56:36 -0500

    added create table function to store.py

--- store.py
+++ store.py
@@ -4,17 +4,52 @@
 '''
 import sqlite3
 from sqlite3 import Error
- 
- 
+
 def create_connection(db_file):
-    """ create a database connection to a SQLite database """
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
+        return conn
     except Error as e:
         print(e)
-    finally:
-        conn.close()
- 
+
+    return None
+
+def create_table(conn, create_table_sql):
+	''' create database table given db connection
+	and sql string to create table
+	: param conn: database connection
+	: param create_table_sql: a CREATE TABLE statement
+	: return: 
+	'''
+	try:
+		c = conn.cursor()
+		c.execute(create_table_sql)
+	except Error as e:
+		print(e)
+
+'''
+Mapping tweet attributes (defined by twitter api) to fields in the tweets table:
+
+	do tweets have a unique identifier? -->	id integer PRIMARY KEY,
+	tweet['user']['screen_name'] 		-->	user varchar(30) NOT NULL,
+	create_date datetime NOT NULL,
+	tweet text NOT NULL,
+	rt bin NOT NULL
+
+create_tweet_table = '''
+CREATE TABLE IF NOT EXISTS tweets (
+	id integer PRIMARY KEY,
+	user varchar(30) NOT NULL,
+	create_date datetime NOT NULL,
+	tweet text NOT NULL,
+	rt bin NOT NULL
+	);'''
+
 if __name__ == '__main__':
     create_connection("C:\\sqlite\db\pythonsqlite.db")
+
